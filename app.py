4b0be12a2fdd6ebc28590d8600c4a90ac6973aae
Loaf0 <ninjakin102@outlook.com>
2024-04-16 15:24:51 -0400

    Added routes to all current webpages

--- app.py
+++ app.py
@@ -3,8 +3,32 @@ from flask import Flask, render_template, request, redirect, url_for, session, a
 app = Flask(__name__)
 
 @app.route("/")
+def home():
+    return "send to main page if not logged in"
+
+@app.route("/login")
 def login():
-    return render_template("portal.html")
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+@app.route("/messages")
+def messages():
+    return render_template("messages.html")
+
+@app.route("/settings")
+def settings():
+    return render_template("settings.html")
+
+@app.route("/notifications")
+def notifications():
+    return render_template("notificiations.html")
 
 if __name__ == '__main__':
     app.run()
