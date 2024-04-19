c4f2bb27179afaa65985b53b4e5c1feb06888b44
Loaf0 <ninjakin102@outlook.com>
2024-04-19 13:40:17 -0400

    Formatting

--- app.py
+++ app.py
@@ -50,38 +50,51 @@ with app.app_context():
 def index():
     return 'Hello, World!'
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
-app = Flask(__name__)
-
 @app.route("/")
 def home():
     return "send to main page if not logged in"
+
 
 @app.route("/login")
 def login():
     return render_template("login.html")
 
+
 @app.route("/register")
 def register():
     return render_template("register.html")
+
 
 @app.route("/profile")
 def profile():
     return render_template("profile.html")
 
+
 @app.route("/messages")
 def messages():
     return render_template("messages.html")
+
 
 @app.route("/settings")
 def settings():
     return render_template("settings.html")
 
+
 @app.route("/notifications")
 def notifications():
     return render_template("notifications.html")
+
+
+# check status of user
+def require_login_status(must_be_logged_out=False, must_be_admin=False, destination='profile'):
+    if 'loggedin' not in session.keys() and not must_be_logged_out:
+        return redirect(url_for('login') + '?destination=' + destination)
+
+    if 'loggedin' in session.keys() and must_be_logged_out:
+        return redirect('/' + destination)
+
+    if must_be_admin and not session['admin']:
+        abort(403)
 
 if __name__ == '__main__':
     app.run()
